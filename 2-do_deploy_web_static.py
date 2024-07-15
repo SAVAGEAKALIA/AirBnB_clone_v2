39b678425a671cf6cca97a0d96c3b5b29c76f2de
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-15 20:12:41 +0100

    Airbnb_V2 Fabric1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -52,54 +52,48 @@ def do_deploy(archive_path):
         print(f"Archive file {archive_path} does not exist.")
         return False
 
-    try:
-        archive_name = os.path.basename(archive_path)
-        # Construct the release directory path
-        release_dir = \
-            f"/data/web_static/releases/{archive_name.replace('.tgz', '')}"
+    archive_name = os.path.basename(archive_path)
+    # Construct the release directory path
+    release_dir = \
+        f"/data/web_static/releases/{archive_name.replace('.tgz', '')}"
 
-        # Iterate over each host defined in Fabric's env.hosts
-        for host in env.hosts:
-            # Upload the archive to /tmp/ directory on the web server
-            print(f"Uploading {archive_path} to {host}...")
-            put(archive_path, '/tmp/')
+    # Iterate over each host defined in Fabric's env.hosts
+    for host in env.hosts:
+        # Upload the archive to /tmp/ directory on the web server
+        print(f"Uploading {archive_path} to {host}...")
+        put(archive_path, '/tmp/')
 
-            # Create the release directory
-            print(f"Creating directory {release_dir} on {host}...")
-            run(f'rm -rf /data/web_static/releases/*')
-            run(f'mkdir -p {release_dir}')
+        # Create the release directory
+        print(f"Creating directory {release_dir} on {host}...")
+        run(f'rm -rf /data/web_static/releases/*')
+        run(f'mkdir -p {release_dir}')
 
-            # Extract the archive contents to the release directory
-            print(f"Extracting {archive_name} on {host}...")
-            run(f'tar -xzf /tmp/{archive_name} -C {release_dir}')
+        # Extract the archive contents to the release directory
+        print(f"Extracting {archive_name} on {host}...")
+        run(f'tar -xzf /tmp/{archive_name} -C {release_dir}')
 
-            # Remove the archive from /tmp/ directory on the web server
-            print(f"Removing {archive_name} from {host}...")
-            run(f'rm -rf /tmp/{archive_name}')
+        # Remove the archive from /tmp/ directory on the web server
+        print(f"Removing {archive_name} from {host}...")
+        run(f'rm -rf /tmp/{archive_name}')
 
-            # Move the contents of the web_static directory to release_dir
-            print(f"Move {release_dir}/web_static* to {release_dir}...")
-            run(f'mv {release_dir}/web_static/* {release_dir}')
+        # Move the contents of the web_static directory to release_dir
+        print(f"Move {release_dir}/web_static* to {release_dir}...")
+        run(f'mv {release_dir}/web_static/* {release_dir}')
 
-            # Remove the old web_static directory from release_dir
-            print(f"Deleting {release_dir}/web_static*...")
-            run(f'rm -rf {release_dir}/web_static')
+        # Remove the old web_static directory from release_dir
+        print(f"Deleting {release_dir}/web_static*...")
+        run(f'rm -rf {release_dir}/web_static')
 
-            # Remove the old release directory from the server
-            print(f"Deleting current symlink on {host}...")
-            run(f'rm -rf /data/web_static/current')
+        # Remove the old release directory from the server
+        print(f"Deleting current symlink on {host}...")
+        run(f'rm -rf /data/web_static/current')
 
-            # Create a new symlink to the new release directory on the server
-            print(f"Creating new symlink on {host}...")
-            run(f'ln -s {release_dir} /data/web_static/current')
+        # Create a new symlink to the new release directory on the server
+        print(f"Creating new symlink on {host}...")
+        run(f'ln -s {release_dir} /data/web_static/current')
 
-            print("New Version deployed successfully.")
-        return True
-
-    except Exception as e:
-        # Handle any exceptions that occur during deployment
-        print(f"Deployment failed: {e}")
-        return False
+        print("New Version deployed successfully.")
+    return True
 
 
 # Entry point when script is run directly from the command line
