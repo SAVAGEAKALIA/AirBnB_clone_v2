9c001a7e2aa3e1aab12519a2bb2636c1fac2e0cd
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-15 08:28:32 +0100

    Airbnb_V2 Fabric

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,32 +19,31 @@ def do_deploy(archive_path):
         print(f"Archive file {archive_path} does not exist.")
         return False
     else:
-        if not argv[1:]:
-            print("ssh_path:argv[5], user:argv[7]")
-        else:
-            try:
-                archive_name = os.path.basename(archive_path)
-                release_dir = \
-                    f"/data/web_static/releases/{archive_name.replace('.tgz', '')}"
+        try:
+            archive_name = os.path.basename(archive_path)
+            print(archive_name)
+            release_dir = \
+                f"/data/web_static/releases/{archive_name.replace('.tgz', '')}"
+            print(release_dir)
 
-                for host in env.host:
-                    print(f"Uploading {archive_path} to {host}...")
-                    put(archive_name, '/tmp/')
+            for host in env.host:
+                print(f"Uploading {archive_path} to {host}...")
+                put(archive_name, '/tmp/')
 
-                    print(f"Extracting {archive_name} on {host}...")
-                    run(f'tar -xzf /tmp/{archive_name} -C {release_dir}')
+                print(f"Extracting {archive_name} on {host}...")
+                run(f'tar -xzf /tmp/{archive_name} -C {release_dir}')
 
-                    print(f"Removing {archive_name} from {host}...")
-                    run(f'rm -rf /tmp/{archive_name}')
+                print(f"Removing {archive_name} from {host}...")
+                run(f'rm -rf /tmp/{archive_name}')
 
-                    print(f"Deleting current symlink on {host}...")
-                    run(f'rm -rf /data/web_static/current')
+                print(f"Deleting current symlink on {host}...")
+                run(f'rm -rf /data/web_static/current')
 
-                    print(f"Creating new symlink on {host}...")
-                    run(f'ln -s {release_dir} /data/web_static/current')
-                print("New Version deployed")
-                return True
+                print(f"Creating new symlink on {host}...")
+                run(f'ln -s {release_dir} /data/web_static/current')
+            print("New Version deployed")
+            return True
 
-            except Exception as e:
-                print(f"Deployment failed: {e}")
-                return False
+        except Exception as e:
+            print(f"Deployment failed: {e}")
+            return False
