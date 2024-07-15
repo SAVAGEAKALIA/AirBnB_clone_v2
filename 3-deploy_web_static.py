7256581cb19e8303aa75ab81db3d1bd1382d32fe
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-15 21:07:59 +0100

    Airbnb_V2 Fabric1

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ Fabric Script to Update Version on webserver """
 
-import os
 from fabric.api import env
 
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
-# env.hosts = ['54.160.101.222', '100.25.205.48']
+env.hosts = ['54.160.101.222', '100.25.205.48']
 # env.user = 'ubuntu'
 # env.key_filename = '/etc/ssh/ssh_config'
 
@@ -19,12 +18,23 @@ def deploy():
     calls the do_deploy function to send to the webserver
     and uncompress files
     """
-
     archive_path = do_pack()
     if not archive_path:
-        return False
+        raise Exception("Packing failed")
 
-    return do_deploy(archive_path)
+    successful_deploy = True
+
+    for host in env.hosts:
+        print(f"Deploying to host: {host}")
+        env.host_string = host
+        result = do_deploy(archive_path)
+        if not result:
+            print(f"Deployment failed for host: {host}")
+            successful_deploy = False
+        else:
+            print(f"Deployment successful for host: {host}")
+
+    return successful_deploy
 
 
 if __name__ == '__main__':
