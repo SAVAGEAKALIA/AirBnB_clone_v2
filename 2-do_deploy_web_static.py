98b4bd1f48b2d8cc66fe38bf44a49c6d9d94d240
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-15 19:00:17 +0100

    Airbnb_V2 Fabric1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -66,6 +66,7 @@ def do_deploy(archive_path):
 
             # Create the release directory
             print(f"Creating directory {release_dir} on {host}...")
+            run(f'rm -rf /data/web_static/releases/{release_dir}')
             run(f'mkdir -p /data/web_static/releases/{release_dir}')
 
             # Extract the archive contents to the release directory
