a3a54ba12838589bc8122b2849ff2aa3cc0e0174
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-15 07:42:11 +0100

    Airbnb_V2 Fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,19 +8,18 @@ from datetime import datetime
 
 def do_pack():
     """ Function to implement generation of tgz files"""
-    # c = Connection('localhost')
     current_time = datetime.now()
     output_file = \
         f"web_static_{current_time.strftime('%Y%m%d%H%M%S')}.tgz"
     if not os.path.exists('versions'):
         os.makedirs(os.path.join('versions', ''))
         output_path = os.path.join('versions', output_file)
-        print("Packing web_static to {}".format(output_path))
+        # print("Packing web_static to {}".format(output_path))
         local(f'tar -cvzf {output_path} web_static')
 
     if os.path.exists('versions'):
-        print("web_static packed: {} -> {}Bytes".
-              format(output_path, os.path.getsize(output_path)))
+        print(f'web_static packed: '
+              f'{output_path} -> {os.path.getsize(output_path)}Bytes')
         # print(f'{os.path.join("versions", output_file)}')
         return os.path.join('versions', output_file)
     else:
