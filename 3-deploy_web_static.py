08573c00477b37af2e956eb51008d605b151e85b
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-15 19:11:36 +0100

    Airbnb_V2 Fabric1

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,8 +8,8 @@ do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 env.hosts = ['54.160.101.222', '100.25.205.48']
-env.user = 'ubuntu'
-env.key_filename = '/etc/ssh/ssh_config'
+# env.user = 'ubuntu'
+# env.key_filename = '/etc/ssh/ssh_config'
 
 
 def deploy():
@@ -19,9 +19,10 @@ def deploy():
     calls the do_deploy function to send to the webserver
     and uncompress files
     """
-    try:
-        archive_path = do_pack()
-    except Exception as e:
-        print(f"Error during packing: {e}")
-        return False
-    return do_deploy(archive_path)
+    for host in env.hosts:
+        try:
+            archive_path = do_pack()
+        except Exception as e:
+            print(f"Error during packing: {e}")
+            return False
+        return do_deploy(archive_path)
