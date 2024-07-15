dd70ace34e41791785bfb583500c6b57ea4af8e6
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-15 20:20:15 +0100

    Airbnb_V2 Fabric1

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,8 +8,8 @@ do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 env.hosts = ['54.160.101.222', '100.25.205.48']
-env.user = 'ubuntu'
-env.key_filename = '/etc/ssh/ssh_config'
+# env.user = 'ubuntu'
+# env.key_filename = '/etc/ssh/ssh_config'
 
 
 def deploy():
