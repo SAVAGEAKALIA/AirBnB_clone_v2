7eb1a86a4da7420defdb5eb2b14c453369dbb8f8
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-15 21:25:07 +0100

    Airbnb_V2 Fabric1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,5 @@
 #!/usr/bin/python3
-"""
-Fabric Script to Deploy and Update Version on Web Server
-
-This script automates the deployment process by uploading a specified archive
-to multiple hosts, extracting it, and updating symbolic links on the servers.
-
-Usage:
-    python3 do_deploy.py <archive_path> <key_filename> <username>
-
-Arguments:
-    archive_path (str): Path to the archive file to deploy.
-    key_filename (str): Path to the SSH private key file for authentication.
-    username (str): Username used to connect to the remote servers.
-
-Environment variables:
-    This script expects the Fabric environment variables 'env.hosts' to be set
-    with a list of host IP addresses or hostnames.
-
-Functions:
-    - do_deploy(archive_path):
-        Deploys the specified archive to remote hosts:
-        - Uploads the archive.
-        - Creates necessary directories.
-        - Extracts the archive.
-        - Moves files to the correct directory structure.
-        - Updates symbolic links.
-"""
+""" Fabric Script to Deploy and Update Version on Web Server """
 
 from fabric.api import env, run, put
 import os
