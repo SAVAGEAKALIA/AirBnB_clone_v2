63c74287897383cacb71f820888c5be2d53be21e
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-15 09:59:01 +0100

    Airbnb_V2 Fabric

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,30 +21,41 @@ def do_deploy(archive_path):
     else:
         try:
             archive_name = os.path.basename(archive_path)
-            print(archive_name)
+            # Construct the release directory path
             release_dir = \
                 f"/data/web_static/releases/{archive_name.replace('.tgz', '')}"
             # print(release_dir)
 
             for host in env.host:
+                # Upload the archive to /tmp/ directory on the web servers
                 print(f"Uploading {archive_path} to {host}...")
                 put(archive_path, '/tmp/')
 
+                # Create the release directory
                 print(f"Creating directory {release_dir} on {host}...")
                 run(f'mkdir -p {release_dir}')
 
+                # Extract the archive contents to the release directory
                 print(f"Extracting {archive_name} on {host}...")
                 run(f'tar -xzf /tmp/{archive_name} -C {release_dir}')
 
+                # Remove the archive from /tmp/ directory on the web servers
                 print(f"Removing {archive_name} from {host}...")
                 run(f'rm -rf /tmp/{archive_name}')
 
+                # Move the contents of the web_static directory to release_dir
                 print(f"Move {release_dir}/web_static* to {release_dir}...")
-                run(f'mv {release_dir/(os.path.join("web_static")}/*, {release_dir}')
+                run(f'mv {release_dir}/web_static/* {release_dir}')
 
+                # Remove the old web_static directory from release_dir
+                print(f"delete {release_dir}/web_static*...")
+                run(f'rm -rf {release_dir}/web_static')
+
+                # Remove the old release directory from the server
                 print(f"Deleting current symlink on {host}...")
                 run(f'rm -rf /data/web_static/current')
 
+                # Create a new symlink to the new release directory on the server
                 print(f"Creating new symlink on {host}...")
                 run(f'ln -s {release_dir} /data/web_static/current')
             print("New Version deployed")
