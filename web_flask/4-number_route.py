47b5c1b6888c57e0c5c0c181fc015456aedb9cda
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-08-01 03:37:01 +0100

    Airbnb_V2 Flask 5

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Route Python function app together with variable text"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    This function handles the route '/c/<text>'
+    and returns a formatted string.
+    Returns 404 error page if no value is given
+    """
+    if text:
+        return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """
+    This function handles the route '/python/<text>'
+    and returns a formatted string.
+    Displays a default value 'cool' if value is given
+    """
+    if text:
+        return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """
+    This function handles the route '/number/<n>'
+    and displays n only if n is a number.
+    Returns 404 error page if no value is not a number.
+    """
+    if n:
+        return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
