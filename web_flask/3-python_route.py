bfb5c8dc662260d3cecf54b36f9fd9459eb5e92d
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-08-01 03:16:51 +0100

    Airbnb_V2 Flask 4

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+""" Route Python function app together with variable text"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    This function handles the route '/c/<text>' and returns a formatted string.
+    Returns 404 error page if no value is given
+    """
+    if text:
+        return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """
+    This function handles the route '/python/<text>' and returns a formatted string.
+    Displays a default value 'cool' if value is given
+    """
+    if text:
+        return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
