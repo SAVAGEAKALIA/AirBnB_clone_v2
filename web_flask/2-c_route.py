bec33711935560e02d3b2508effe34fc1f3f302e
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-08-01 02:54:10 +0100

    Airbnb_V2 Flask 3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Route C function app together with variable text"""
+from flask import Flask
+import sys
+
+if len(sys.argv) > 3:
+    arg = sys.argv[4].replace('_', ' ')
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    This function handles the route '/c/<text>' and returns a formatted string.
+    """
+    if text:
+        return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
